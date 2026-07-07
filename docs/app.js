0ae53ff59654b06bc53ea33e7628ffb2b4dc3bce
9dgvk7ky68-dev <9dgvk7ky68@privaterelay.appleid.com>
2026-07-07 04:00:26 +0000

    Use Monday as week start for fortnights

--- docs/app.js
+++ docs/app.js
@@ -73,8 +73,8 @@ function saveState() {
 function getMondayOfCurrentWeek(reference = new Date()) {
   const date = new Date(reference);
   const day = date.getDay();
-  const diff = day === 0 ? -6 : 1 - day;
-  date.setDate(date.getDate() + diff);
+  const diff = day === 0 ? -6 : day - 1;
+  date.setDate(date.getDate() - diff);
   date.setHours(0, 0, 0, 0);
   return date;
 }
