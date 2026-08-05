9e5febb3b7af9d3442c1d6eccc6ca4500e4b82ca
9dgvk7ky68-dev <9dgvk7ky68@privaterelay.appleid.com>
2026-08-05 02:00:06 +0000

    Improve mobile tab nav visibility and pinning

--- balancetrack-source/frontend/sw.js
+++ balancetrack-source/frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'balancetrack-v10';
+const CACHE_NAME = 'balancetrack-v11';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.svg', './icons/icon-512.svg'];
 
 self.addEventListener('install', (event) => {
