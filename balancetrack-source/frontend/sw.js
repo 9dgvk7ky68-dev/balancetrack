cf18836f0b5bc94df50085ee54a3ddbcc9c81479
9dgvk7ky68-dev <9dgvk7ky68@privaterelay.appleid.com>
2026-08-05 02:02:40 +0000

    Use icon-only fixed bottom nav

--- balancetrack-source/frontend/sw.js
+++ balancetrack-source/frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'balancetrack-v11';
+const CACHE_NAME = 'balancetrack-v12';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.svg', './icons/icon-512.svg'];
 
 self.addEventListener('install', (event) => {
